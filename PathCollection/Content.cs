489b559fd9e7ad1a47342adce0ab90a05fb70daa
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 20:18:48 +0300

    Add project files.

---
+++ PathCollection/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathCollection
+{
+    public abstract class Content
+    {
+        private static List<string> paths = new List<string>();
+        private static List<string> names = new List<string>();
+
+        public static List<string> Paths
+        {
+            get { return paths; }
+        }
+
+        public static List<string> Names
+        {
+            get { return names; }
+        }
+    }
+}
---
+++ PathCollection/DirectoryScheme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathCollection
+{
+    public class DirectoryScheme
+    {
+        private List<DirectoryScheme> childrenDirectorySchemes;
+        private List<string> childrenFilePaths;
+        private string path;
+        private DateTime lastWriteTime;
+
+        private void Update()
+        {
+            DirectoryInfo currentDirectoryInfo = new DirectoryInfo(path);
+
+            DirectoryInfo[] childrenDirectoryInfos = currentDirectoryInfo.GetDirectories();
+            FileInfo[] childrenFileInfos = currentDirectoryInfo.GetFiles();
+
+            if (childrenDirectoryInfos.Length>childrenDirectorySchemes.Count)
+            {
+                for (int i=0;i<childrenDirectorySchemes.Count;i++)
+                {
+
+                }
+            }
+        }
+
+        public void Check(DateTime checkTime)
+        {
+            if (!lastWriteTime.Equals(checkTime))
+            {
+                lastWriteTime = Directory.GetLastWriteTime(path);
+                Update();
+            }
+        }
+    }
+}
